3e9372bdd1f67c5cf03ef639315925a3aa4a7f57
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-24 18:43:34 +0000

    Chop component created

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './Shop.css';
+import React from "react";
+import "./Shop.css";
 
 const Shop = () => {
-    return (
-        <div>
-            <h2>This is Shop</h2>
-        </div>
-    );
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h3>This is Shop</h3>
+      </div>
+      <div className="cart-container">
+        <h4>This is Cart</h4>
+    <h4>This is Cart</h4>
+        <h4>This is Cart</h4>
+        <h4>This is Cart</h4>
+        <h4>This is Cart</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
